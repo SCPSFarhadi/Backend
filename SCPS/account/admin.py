5b6e8b866c03f58e10e3aaf89d5c5b15db6121b9
pouria <arefijamal.pouria@gmail.com>
2022-02-15 13:04:00 +0330

    role added

--- SCPS/account/admin.py
+++ SCPS/account/admin.py
@@ -12,7 +12,7 @@ class UserAdminConfig(UserAdmin):
     list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
     ordering = ('-start_date',)
     list_display = ('email', 'id','user_name', 'first_name',
-                    'is_active', 'is_staff')
+                    'is_active', 'is_staff', 'role')
     fieldsets = (
         (None, {'fields': ('email', 'user_name', 'first_name',)}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
